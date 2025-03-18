4f530e5188304040d4af66b04641afd5e683af03
josedherna <157747148+josedherna@users.noreply.github.com>
2025-03-17 20:34:51 -0400

    init commit

---
+++ src/main/java/org/csc311/weatherdata/Main.java
@@ -0,0 +1,17 @@
+package org.csc311.weatherdata;
+
+/**
+ * This class reads data from a csv that contains weather data and analyzes it.
+ *
+ * @author Jose Hernandez
+ */
+public class Main {
+    /**
+     * This method is the entry point to execute a Java program.
+     *
+     * @param args An array that contains strings of arguments received from command-line.
+     */
+    public static void main(String[] args) {
+
+    }
+}
